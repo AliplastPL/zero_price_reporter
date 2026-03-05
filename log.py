60f8df4f34fd6555bf43c78e0eb4e8e8e42c8fc7
j.oska <jakub.oska@aliplast.pl>
2026-03-05 12:36:39 +0100

    Console fix

--- log.py
+++ log.py
@@ -19,10 +19,13 @@ current_dateTime = time.strftime("%d.%m.%Y_%H-%M-%S", time.localtime())
 log_file_path = os.path.join(logs_dir, f"{current_dateTime}.log")
 
 def printandlog(message):
-    print(message)
+    try:
+        print(message)
+    except (OSError, AttributeError):
+        pass  # Console is not available
 
     try:
         with open(log_file_path, "a", encoding='utf-8') as f:
             f.write(f"{message}\n")
     except Exception as e:
-        print(f"Błąd podczas zapisu do loga: {str(e)}")
+        pass
